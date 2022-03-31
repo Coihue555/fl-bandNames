7d359e172e806f923b6616523bd695ff9085acb0
Coihue555 <kevlar555@gmail.com>
2022-03-31 12:01:05 -0300

    yey

--- 01-fl_band_names_server/index.js
+++ 01-fl_band_names_server/index.js
@@ -1,1 +1,30 @@
 const express =  require('express');
+const path = require('path');
+require('dotenv').config();
+//App de Express
+const app = express();
+
+
+//Node Server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Mensajes de sockets
+io.on('connection', client => {
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado')
+    });
+  });
+
+
+
+//path publico
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use( express.static(publicPath) );
+
+
+server.listen(process.env.PORT, (err)=>{
+    if (err) throw new Error(err);
+    console.log('Servidor corriendo en puertioo: ', process.env.PORT);
+});
